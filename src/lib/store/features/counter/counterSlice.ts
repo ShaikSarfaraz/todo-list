a7bceac196c3cb561a49e5e91aeaf08cdec87585
Sarfaraz Shaik <63334137+ShaikSarfaraz@users.noreply.github.com>
2025-02-21 15:06:51 +0530

    Removed unneccesary comments

--- src/lib/store/features/counter/counterSlice.ts
+++ src/lib/store/features/counter/counterSlice.ts
@@ -24,22 +24,6 @@ export const counterSlice = createSlice({
   },
 })
 
-// export const counterSlice = createSlice({
-//   name: "counter",
-//   initialState,
-//   reducers: {
-//     increment: (state) => {
-//       state.value += 1
-//     },
-//     decrement: (state) => {
-//       state.value -= 1
-//     },
-//     incrementByAmount: (state, action: PayloadAction<number>) => {
-//       state.value += action.payload
-//     },
-//   },
-// })
-
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
 export default counterSlice.reducer
 
