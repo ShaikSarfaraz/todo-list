784db4740855da1c671e902ab49547b07f3702bc
Sarfaraz Shaik <sarfarazshaik2807@gmail.com>
2025-03-03 22:47:04 +0530

    Commented the little things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        {/* <Providers>{children}</Providers> */}
       </body>
     </html>
   )
